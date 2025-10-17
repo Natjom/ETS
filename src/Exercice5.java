9f85548df96a92aebfe9098a84ff781d6e4cb217
natjom <n4t10m@gmail.com>
2025-10-17 11:02:03 +0200

    .

--- src/Exercice5.java
+++ src/Exercice5.java
@@ -1,5 +1,5 @@
 void main() {
-    int[] tableau = {1, 2, 2, 2, 3, 4, 5};
+    int[] tableau = {1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 20, 20, 21};
     System.out.println(rechPremOcc(tableau, 2));
 }
 
