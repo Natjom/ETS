852a7a8586591a343cac31291f5b37a26133ebae
natjom <n4t10m@gmail.com>
2025-10-17 12:29:10 +0200

    exo 8

--- src/Exercice8.java
+++ src/Exercice8.java
@@ -11,10 +11,22 @@ void main() {
 
         int val = 1;
         for (int j = 0; j <= i; j++) {
-            System.out.printf("%3s ", Integer.toHexString(val).toUpperCase());
+            System.out.print(" " + val + "  ");
             val = val * (i - j) / (j + 1);
         }
         System.out.println();
+    }
+
+    int i = n - 1;
+    int val = 1;
+
+    System.out.print("\nDéveloppement de (a + b)^" + i + " : ");
+    for (int j = 0; j <= i; j++) {
+        if (val != 1) System.out.print(val);
+        if (i - j > 0) System.out.print("a" + (i - j > 1 ? "^" + (i - j) : ""));
+        if (j > 0) System.out.print("b" + (j > 1 ? "^" + j : ""));
+        if (j < i) System.out.print(" + ");
+        val = val * (i - j) / (j + 1);
     }
 
     sc.close();
