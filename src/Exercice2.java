49e63adc8ab3fd900fb30f5ff08f5ac3d411cdff
natjom <n4t10m@gmail.com>
2025-10-17 09:23:01 +0200

    Exo 2 good (easy)

--- src/Exercice2.java
+++ src/Exercice2.java
@@ -1,35 +1,40 @@
-
-// Analysez le problème suivant et choisissez la structure de contrôle la plus appropriée :
-// Problème : Créer un programme qui calcule la moyenne des notes d’un étudiant.
-// Le nombre de notes n’est pas connu à l’avance, et l’utilisateur saisit pour terminer la saisie.
-
 // Questions d’analyse :
+
 // Quelle structure répétitive convient le mieux : POUR, TANT QUE, ou REPETER...JUSQU’ A ?
+// POUR
+
 // Justifiez votre choix
+// Flemme, c'est de la logique en fait : Un while est trop gourmand alors qu'un pour est précis tu peux faire un "POUR NOMBRENOTESENTREES" fin bref
+
 // Identifiez les variables nécessaires et leur rôle
+// Un stockage des notes et le nombre de notes c'est tout (et aussi la "variable lue temporaire mais ça dépend du language)
+
 // Proposez l’algorithme complet
+// Voici :
 
 import java.util.Scanner;
 
 public class Exercice2 {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Entrez votre nom : ");
-        String nom = scanner.nextLine();
+        System.out.print("Combien de notes avez vous : ");
+        int nombreNotes = scanner.nextInt();
 
-        System.out.print("Entrez votre âge : ");
-        int age = scanner.nextInt();
+        int num = 0;
 
-        System.out.println("Bienvenue, " + nom + " !");
-
-        if (age >= 18) {
-            System.out.println("Vous êtes majeur(e)");
-        } else {
-            System.out.println("Vous êtes mineur(e)");
+        for (int i = 0; i < nombreNotes; i++) {
+            System.out.print("Quelle est votre note numéro " +  (i + 1) + " : ");
+            num = num + scanner.nextInt();
         }
+
+        System.out.print("Votre moyenne est de : " + num/nombreNotes + " !");
+
+
 
         scanner.close();
     }
 }
+
+
+
